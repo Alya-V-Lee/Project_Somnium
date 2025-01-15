25d009868340a7952759e3aee49c1ce2d8a2a94b
Alya-V-Lee <alya.v.lee@gmail.com>
2025-01-14 20:06:54 -0500

    Expanded MainPlayerController functionality to achieve player movement. Created MainGameModeBase class. Created Blueprints derived from both and set variables in editor. Added camera and movement configurations to MainCharacter constructor.

--- Source/Project_Somnium/Private/Character/MainCharacter.cpp
+++ Source/Project_Somnium/Private/Character/MainCharacter.cpp
@@ -2,4 +2,31 @@
 
 
 #include "Character/MainCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/ArrowComponent.h"
 
+AMainCharacter::AMainCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+	
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("Spring Arm");
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->SetRelativeRotation(FRotator(-30.0f, 90.0f, 0.0f));
+	SpringArm->bUsePawnControlRotation = false;
+	SpringArm->TargetArmLength = 700.f;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritYaw = false;
+	SpringArm->bInheritRoll = false;
+	
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Camera->SetupAttachment(SpringArm);
+}
