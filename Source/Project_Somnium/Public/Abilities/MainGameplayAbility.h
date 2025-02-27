022726c87c07451c3336bec5add420921d783c24
Alya-V-Lee <alya.v.lee@gmail.com>
2025-02-27 14:34:29 -0500

    Implemented Attribute Change Broadcasting and Initial Gameplay Ability System * Updated AttributeMenuWidgetController  - Implemented dynamic broadcasting for Attribute changes using GetGameplayAttributeValueChangeDelegate().  - Added for loop to bind Attribute Change Delegates for all Attributes in TagsToAttributes.  - Added Lambda inside loop to capture Attribute updates and broadcast them dynamically.  - Refactored repeated Attribute broadcasting code into new BroadcastAttributeInfo() function. * Updated MainAttributeSet  - Added Secondary Attributes to TagsToAttributes map for dynamic retrieval. * Introduced MainGameplayAbility (Base Gameplay Ability Class)  - New parent class for all future Gameplay Abilities. * Updated MainCharacterBase  - Introduced StartupAbilities array to store initial Gameplay Abilities.  - Added AddCharacterAbilities() function to grant abilities only on the server.  - Called AddCharacterAbilities() inside PossessedBy() to ensure Abilities are granted when MainCharacter actors are possessed. * Updated MainAbilitySystemComponent  - Added AddCharacterAbilities() function to grant Startup Abilities dynamically.

---
+++ Source/Project_Somnium/Public/Abilities/MainGameplayAbility.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "MainGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_SOMNIUM_API UMainGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
